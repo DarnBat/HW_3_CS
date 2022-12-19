78241ced0023a68237f99a9c3ada9a9c504538b1
Vladislav.M <lidpid@gmail.com>
2022-12-19 20:14:54 +0600

    task 19 completed

---
+++ Task_19/Program.cs
@@ -0,0 +1,13 @@
+﻿// Task 19
+Console.WriteLine("Введите пятизначное число");
+string? a = (Console.ReadLine());
+while(a.Length != 5){
+    Console.WriteLine("Введите ПЯТИЗНАЧНОЕ число");
+    a = (Console.ReadLine());
+}
+if(a[0] == a[4] && a[1] == a[3]){
+    Console.WriteLine("Да");
+}
+else{
+    Console.WriteLine("Нет");
+}
