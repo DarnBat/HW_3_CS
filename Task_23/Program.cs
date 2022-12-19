1fc7003d73e49c116caf19a86d4fb621376fe1f4
Vladislav.M <lidpid@gmail.com>
2022-12-19 21:18:13 +0600

    task 23 completed

---
+++ Task_23/Program.cs
@@ -0,0 +1,8 @@
+﻿// Task 23
+Console.Clear();
+Console.WriteLine("Введите число");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+for(int i = 1; i < n + 1; i++){
+    Console.WriteLine($"{Math.Pow(i, 3)}");
+}
