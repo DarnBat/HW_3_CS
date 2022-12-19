da0e8f973ab9f497092c089a4f03ecf60226ed87
Vladislav.M <lidpid@gmail.com>
2022-12-19 19:32:46 +0600

    task 21 completed

---
+++ Task_21/Program.cs
@@ -0,0 +1,20 @@
+﻿// Task 21
+Console.Clear();
+Console.WriteLine("Введите координату x1");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите координату y1");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите координату z1");
+double z1 = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите координату x2");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите координату y2");
+double y2 = Convert.ToDouble(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите координату z2");
+double z2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2)));
